fe2da31e5e0fa3bb64349d59f211a8a9e72c8378
MisbahHamid <misbahhamid74@gmail.com>
2024-08-18 11:37:31 +0500

    sixth commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int length = 36;
-        int breath = 36;
-        int radius = 36;
-        double area_of_circle = 3.14*radius*radius;
-        double perimeter_of_circle = 2*3.14*radius;
-        int area_of_rectangle = length*breath;
-        int perimeter_of_recantgle = (length+breath)*2;
-        System.out.println("the Area of circle is =" + area_of_circle);
-        System.out.println("Perimeter of Circle = " + perimeter_of_circle);
-        System.out.println("Area of Rectangle is = " + area_of_rectangle);
-        System.out.println("Perimeter of rectangle is = " + perimeter_of_recantgle);
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the first positive number (base): ");
+        double base = scanner.nextDouble();
+
+
+        System.out.print("Enter the second positive number (exponent): ");
+        double exponent = scanner.nextDouble();
+        double result= Math.pow(base,exponent);
+
+
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+
+        scanner.close();
     }
 }
+
+
