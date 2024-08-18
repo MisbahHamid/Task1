194dc301b0c8ba503b1ac64413c2c220b880ea3f
MisbahHamid <misbahhamid74@gmail.com>
2024-08-18 11:35:33 +0500

    fifth commit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int Fahrenheit=108;
-        int CentigradeDegree= (Fahrenheit-32)*5/9;
-        System.out.println("the temperature in Fahrenheit is =" + Fahrenheit);
-        System.out.println("the temperature in centigrade Degree is = "+ CentigradeDegree);
+        int length = 36;
+        int breath = 36;
+        int radius = 36;
+        double area_of_circle = 3.14*radius*radius;
+        double perimeter_of_circle = 2*3.14*radius;
+        int area_of_rectangle = length*breath;
+        int perimeter_of_recantgle = (length+breath)*2;
+        System.out.println("the Area of circle is =" + area_of_circle);
+        System.out.println("Perimeter of Circle = " + perimeter_of_circle);
+        System.out.println("Area of Rectangle is = " + area_of_rectangle);
+        System.out.println("Perimeter of rectangle is = " + perimeter_of_recantgle);
 
     }
 }
