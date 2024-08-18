19a03bd2ed676d514a1dd1e8e11f8e12eca691d4
MisbahHamid <misbahhamid74@gmail.com>
2024-08-18 11:34:14 +0500

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int TotalMarks=100;
-        int subject1= 80;
-        int subject2=90;
-        int subject3=75;
-        int subject4=60;
-        int subject5=100;
-        int total = subject1+subject2+subject3+subject3+subject4+subject5;
-        int aggregate=0;
-        aggregate+=total;
-        int percentage =total*100/100;
-        System.out.println("the total marks are" + TotalMarks);
-        System.out.println("Aggrigate is " + aggregate);
-        System.out.println("Percentage is " + percentage);
+        int Fahrenheit=108;
+        int CentigradeDegree= (Fahrenheit-32)*5/9;
+        System.out.println("the temperature in Fahrenheit is =" + Fahrenheit);
+        System.out.println("the temperature in centigrade Degree is = "+ CentigradeDegree);
+
     }
 }
